bf07ac798a5bd902e5bc94a5ac6cfaad66429966
saiganesh4299 <68476721+saiganesh4299@users.noreply.github.com>
2020-10-24 17:31:25 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -3,8 +3,12 @@ int main() {
 
     int number1, number2, sum;
     
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    printf("Enter 1st integer: ");
+    scanf("%d", &number1,);
+    
+    printf("Enter 2nd integer: ");
+    scanf("%d", &number2,);
+
 
     // calculating sum
     sum = number1 + number2;      
